b2430daf4feea9a3502672456e8a51f757fa5edd
Srikanth Hebbar M P <srikanth.hebbar14@outlook.com>
2020-08-26 20:59:27 +0530

    Added countup

--- src/components/LeftSide/CardInfo.js
+++ src/components/LeftSide/CardInfo.js
@@ -1,16 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import CountUp from "react-countup";
 
 const CardInfo = ({ title, fontType, casesNumber }) => {
   //Numbering in Indian format
-  const curformat = new Intl.NumberFormat("en-IN");
+  //const curformat = new Intl.NumberFormat("en-IN");
 
   return (
     <div className="col m4" style={cardStyle}>
       <div className="card-panel white" style={{ height: "150px" }}>
         <span className={fontType}>{title}</span>
         <div className={fontType} style={{ marginTop: "25%" }}>
-          {curformat.format(casesNumber)}
+          <CountUp end={casesNumber} separator="," duration={3} />
         </div>
       </div>
     </div>
